ce20d5fdf7182393e8474193fe36056eb5305ce1
manikandang14study <manikandang14study@gmail.com>
2021-09-01 19:40:31 +0530

    A

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = new express();
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 app.get('/', (req, res) => {
-    res.send("WElcome");
+    res.send("WElcome to dicker ExpressJs");
 });
 
 app.listen(port, () => {
