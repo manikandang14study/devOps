a6eba5226b1b0bed05c721604e285a85250f656d
manikandang14study <manikandang14study@gmail.com>
2021-09-15 19:10:07 +0530

    uncaughtException

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = new express();
 const port = process.env.PORT || 3000
 app.get('/', (req, res) => {
-    res.send("WElcome to dicker ExpressJs");
+    res.send("Welcome to docker ExpressJs");
 });
 
 app.listen(port, () => {
     console.log(`Server started on ${port}`);
+});
+process.on('uncaughtException', (err) => {
+    err ? console.log(`Error: uncaughtException ${err}`) : null
 });
