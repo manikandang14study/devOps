fd996026cadd58432d456cb25a9ceb2d652ed099
manikandang14study <manikandang14study@gmail.com>
2021-09-19 19:05:58 +0530

    Docker Compose

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,6 @@ FROM node:10
 WORKDIR /app
 COPY package.json /app
 RUN npm install
-COPY . /app
+COPY ./ ./
+EXPOSE 8000
 CMD node app.js
-EXPOSE 3000
--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 const express = require('express');
-const app = new express();
-const port = process.env.PORT || 3000
+const redis = require('redis');
+const app = express();
+const port = process.env.PORT || 8000
+
+const client = redis.createClient({
+    host: "redis-server", port: 6379
+});
+client.set('visits', 0)
 app.get('/', (req, res) => {
-    res.send("Welcome to docker ExpressJs");
+    // process.exit(0);
+    client.get('visits', (err, val) => {
+        console.log(`Visits count: ${val}`);
+        res.send(`Visits count: ${val}`);
+        client.set('visits', parseInt(val) + 1)
+    })
 });
 
 app.listen(port, () => {
-    console.log(`Server started on ${port}`);
+    console.log(`Server started on port ${port}`);
 });
+
 process.on('uncaughtException', (err) => {
-    err ? console.log(`Error: uncaughtException ${err}`) : null
-});
+    if (err) {
+        console.log(err);
+        process.exit(0);
+    }
+})
